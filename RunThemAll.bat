5025efa3bd5106fba8f15597676197ac99347356
Li <136283672+JM-2023@users.noreply.github.com>
2024-05-01 11:57:08 +0800

    Add files via upload

---
+++ RunThemAll.bat
@@ -0,0 +1,6 @@
+PROMPT $G
+
+SimpCode.py ExampleProgram/hello_world.simp
+SimpCode.py ExampleProgram/basic_calculation.simp
+SimpCode.py ExampleProgram/for_loop.simp
+SimpCode.py ExampleProgram/while_loop.simp
